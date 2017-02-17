cbfc82ffb6325ee84db7172704f3124afc994d41
eyece <fredrichawk@gmail.com>
2017-02-17 07:09:41 +0100

    Added html handling through webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: './dist/app.bundle.js'
-  }
+    path: 'dist',
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Twitch App',
+      minify: {
+        collapseWhitepsace: false // Set to true to minify html
+      },
+      hash: true, // Set to true if we want hashed bundles.
+      template: './src/index.html', // Load a custom template (ejs by default)
+    })
+    ]
 }

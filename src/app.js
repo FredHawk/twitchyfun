66f21352a45bfd5e44edaa37eb5e59487a5584b2
eyece <fredrichawk@gmail.com>
2017-02-23 07:23:03 +0100

    Refactored so the get data us done by passing in more information

--- src/app.js
+++ src/app.js
@@ -2,24 +2,32 @@ import css from './style.css';
 
 const channels = ["brunofin", "comster404", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-function getData (url) {
+function getData (type, channel) {
+  const url = `https://wind-bow.gomix.me/twitch-api/${type}/${channel}`;
   let result = [];
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      // if (url.includes('streams')) {
-      //   return result.push({'stream': data.stream, 'links': data._links});
-      // }
+      // console.log(data.stream);
+
+      if (url.includes('streams')) {
+        if (data.stream != null) {
+          let temp = result.push({'stream': data.stream, 'links': data._links});
+          console.log(result);
+          return temp;
+        }
+      }
       return result.push(data)
-    });
+    })
+    .catch(error => console.error(error));
   
   return result;
 }
 
 function getStream(channel) {
-  const urlStreams = `https://wind-bow.gomix.me/twitch-api/streams/${channel}`;
+  // const urlStreams = `https://wind-bow.gomix.me/twitch-api/streams/${channel}`;
 
-  const data = getData(urlStreams)
+  const data = getData('streams', channel)
   return data;
 }
 
@@ -35,10 +43,10 @@ function getChannels(channel) {
 // }
 
 const streamData = channels.map(getStream);
-const channelData = channels.map(getChannels);
+// const channelData = channels.map(getChannels);
 
 // const offlineStreams = streamData.filter(streamData => streamData.Object.stream === null);
 
-console.log(streamData);
-console.log(channelData);
+// console.log(streamData);
+// console.log(channelData);
 // console.log(offlineStreams);

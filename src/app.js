61f5fb94b74d0797647591261576e5f991382536
eyece <fredrichawk@gmail.com>
2017-02-25 13:30:42 +0100

    Can now get streams that are online

--- src/app.js
+++ src/app.js
@@ -15,16 +15,35 @@ function getData (type, channel) {
     .then(data => {
       
       if (data.stream != null) {
-        console.log(data.stream.channel.name);
+        // console.log(data.stream.channel.name);
+        // Show online channels
+        const chans = getURL('channels', data.stream.channel.name);
+        fetch(chans)
+        .then(response => response.json())
+        .then(data => console.log('online: ', data))
+        .catch(err => console.error(err));
+      } else if (data.stream == null) {
+        // console.log('Streamdata: ', data.stream);
+        // Show offline channels
+        const chans = getURL('channels', data.stream.channel.name);
+        fetch(chans)
+        .then(response => response.json())
+        .then(data => console.log('offline: ', data.stream.channel.name))
+        .catch(err => console.error(err));
       } else {
-        console.log('Streamdata: ', data.stream);
+        // show all channels
+        const chans = getURL('channels', data.stream.channel.name);
+        fetch(chans)
+        .then(response => response.json())
+        .then(data => console.log('Channeldata: ', data))
+        .catch(err => console.error(err));
       }
       // get channels data
-      const chans = getURL('channels', channel);
-      fetch(chans)
-      .then(response => response.json())
-      .then(data => console.log('Channeldata: ', data))
-      .catch(err => console.error(err));
+      // const chans = getURL('channels', channel);
+      // fetch(chans)
+      // .then(response => response.json())
+      // .then(data => console.log('Channeldata: ', data))
+      // .catch(err => console.error(err));
     })
     .catch(err => console.error(err));
 }

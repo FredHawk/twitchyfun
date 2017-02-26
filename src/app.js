d01e033ff836067091e4376126d6730342433d49
eyece <fredrichawk@gmail.com>
2017-02-26 12:00:06 +0100

    got the html into an array

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ import css from './style.css';
 const allButton = document.querySelector('.btn__all');
 const onlineButton = document.querySelector('.btn__online');
 const offlineButton = document.querySelector('.btn__offline');
+const resultList = document.querySelector('.result');
 
 const channels = ["brunofin", "comster404", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+let results = [];
 
 function getURL(type, channel) {
   const baseURL = `https://wind-bow.gomix.me/twitch-api`;
@@ -13,6 +15,7 @@ function getURL(type, channel) {
 
 function getData (type, channel, status) {
   // get streams data
+  
   const streams = getURL(type, channel);
   fetch(streams)
     .then(response => response.json())
@@ -20,12 +23,11 @@ function getData (type, channel, status) {
       if (status == 'online') {
         if (data.stream != null) {
           // Show online channels
-          status = 'online';
           const chans = getURL('channels', channel);
           fetch(chans)
           .then(response => response.json())
           .then(data => {
-            console.log('online: ', data)
+            console.log('online: ', data);
           })
           .catch(err => console.error(err));
         }
@@ -35,7 +37,9 @@ function getData (type, channel, status) {
         const chans = getURL('channels', channel);
         fetch(chans)
         .then(response => response.json())
-        .then(data => console.log('offline: ', data))
+        .then(data => {
+            console.log('offline: ', data);
+        })
         .catch(err => console.error(err));
         }
       } else {
@@ -43,29 +47,40 @@ function getData (type, channel, status) {
         const chans = getURL('channels', channel);
         fetch(chans)
         .then(response => response.json())
-        .then(data => console.log('Channeldata: ', data))
+        .then(data => {
+            console.log('all: ', data);
+            const channelName = data.display_name ? data.display_name : channel;
+            console.log(channelName);
+            const channelLogo = data.logo ? data.logo : channel
+            // setup a string to display the html.
+            const html = `
+              <li class="result">
+                <img src="${channelLogo}" />
+                <a href="${data.url ? data.url : ''}"><h3>${channelName}</h3></a>
+                <p>${data.status ? data.status == '404' ? 'Channel doesn\'t exist' : data.status : ''}</p>
+              </li>
+            `;
+            // console.log(html);
+            // channel.forEach(results.push(html));
+            results.push(html);
+            // resultList.innerHTML = html;
+            // console.log(results);
+        })
         .catch(err => console.error(err));
+        // console.log(results);
+
       }
     })
     .catch(err => console.error(err));
 }
-function getOnline() {
-  console.log('From online');
-  channels.map((channel, status ) => getData('streams', channel, 'online'));
-}
-function getOffline() {
-  console.log('From offline');
-  channels.map((channel, status ) => getData('streams', channel, 'offline'));
-}
-function getAll() {
-  console.log('From all');
-  channels.map((channel, status ) => getData('streams', channel));
-}
 
 function getInfo(status) {
-  console.log(`From ${status}`);
-  let streamStatus = status
+  let streamStatus = status;
   channels.map((channel, status ) => getData('streams', channel, streamStatus));
+  console.log(results);
+  const html = results.toString();
+  console.log(html);
+  // resultList.innerHTML = html;
 }
 
 onlineButton.addEventListener('click', () => getInfo('online'));

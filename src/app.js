896a5dd624f746bcedfd22cbcb74120ec97bdd40
eyece <fredrichawk@gmail.com>
2017-02-26 13:42:35 +0100

    Added default state of loading All

--- src/app.js
+++ src/app.js
@@ -71,3 +71,5 @@ function getInfo(status) {
 onlineButton.addEventListener('click', () => getInfo('online'));
 offlineButton.addEventListener('click', () => getInfo('offline'));
 allButton.addEventListener('click', () => getInfo('all'));
+
+window.onload = () => getInfo('all');

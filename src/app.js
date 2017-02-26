a8235735975f912698e9105f96279a54f1e360a8
eyece <fredrichawk@gmail.com>
2017-02-26 10:54:47 +0100

    Got the buttons to work with online, offline, all.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import css from './style.css';
 
+const allButton = document.querySelector('.btn__all');
+const onlineButton = document.querySelector('.btn__online');
+const offlineButton = document.querySelector('.btn__offline');
+
 const channels = ["brunofin", "comster404", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 function getURL(type, channel) {
@@ -7,44 +11,63 @@ function getURL(type, channel) {
   return `${baseURL}/${type}/${channel}`;
 }
 
-function getData (type, channel) {
+function getData (type, channel, status) {
   // get streams data
   const streams = getURL(type, channel);
   fetch(streams)
     .then(response => response.json())
     .then(data => {
-      
-      if (data.stream != null) {
-        // console.log(data.stream.channel.name);
-        // Show online channels
-        const chans = getURL('channels', data.stream.channel.name);
-        fetch(chans)
-        .then(response => response.json())
-        .then(data => console.log('online: ', data))
-        .catch(err => console.error(err));
-      } else if (data.stream == null) {
-        // console.log('Streamdata: ', data.stream);
+      if (status == 'online') {
+        if (data.stream != null) {
+          // Show online channels
+          status = 'online';
+          const chans = getURL('channels', channel);
+          fetch(chans)
+          .then(response => response.json())
+          .then(data => {
+            console.log('online: ', data)
+          })
+          .catch(err => console.error(err));
+        }
+      } else if(status == 'offline') {
+        if (data.stream == null) {
         // Show offline channels
-        const chans = getURL('channels', data.stream.channel.name);
+        const chans = getURL('channels', channel);
         fetch(chans)
         .then(response => response.json())
-        .then(data => console.log('offline: ', data.stream.channel.name))
+        .then(data => console.log('offline: ', data))
         .catch(err => console.error(err));
+        }
       } else {
         // show all channels
-        const chans = getURL('channels', data.stream.channel.name);
+        const chans = getURL('channels', channel);
         fetch(chans)
         .then(response => response.json())
         .then(data => console.log('Channeldata: ', data))
         .catch(err => console.error(err));
       }
-      // get channels data
-      // const chans = getURL('channels', channel);
-      // fetch(chans)
-      // .then(response => response.json())
-      // .then(data => console.log('Channeldata: ', data))
-      // .catch(err => console.error(err));
     })
     .catch(err => console.error(err));
 }
-channels.map(channel => getData('streams', channel));
+function getOnline() {
+  console.log('From online');
+  channels.map((channel, status ) => getData('streams', channel, 'online'));
+}
+function getOffline() {
+  console.log('From offline');
+  channels.map((channel, status ) => getData('streams', channel, 'offline'));
+}
+function getAll() {
+  console.log('From all');
+  channels.map((channel, status ) => getData('streams', channel));
+}
+
+function getInfo(status) {
+  console.log(`From ${status}`);
+  let streamStatus = status
+  channels.map((channel, status ) => getData('streams', channel, streamStatus));
+}
+
+onlineButton.addEventListener('click', () => getInfo('online'));
+offlineButton.addEventListener('click', () => getInfo('offline'));
+allButton.addEventListener('click', () => getInfo('all'));
--- src/app2.js
+++
@@ -1,4 +0,0 @@
-function read() {
-  console.log('Hi from app2');
-}
-export default read;
